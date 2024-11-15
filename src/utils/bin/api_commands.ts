816be422d65472b3442bf0e39e03ed900496f822
Jugnu Misal <jugnumisal@gmail.com>
2024-11-15 15:54:17 -0600

    Adding more commands

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -17,8 +17,7 @@ export const projects = async (args: string[]): Promise<string> => {
 
 export const readme = async (args: string[]): Promise<string> => {
   const readme = await getReadme();
-  return `Opening GitHub README...\n
-  ${readme}`;
+  return `${readme}`;
 };
 
 export const weather = async (args: string[]): Promise<string> => {
