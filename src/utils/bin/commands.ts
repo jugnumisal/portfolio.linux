54f574418e086e8e5969447ee09408ac22d02ea4
Jugnu Misal <jugnumisal@gmail.com>
2024-11-19 22:29:18 -0600

    cd condition for empty argument

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -103,7 +103,7 @@ export const cd = async (args: string[]): Promise<string> => {
   if (args[0] === 'secrets_of_the_universe') {
     return `The answer is 42!`;
   }
-  if (args[0] === '') {
+  if (args.length === 0 || args[0] === '') {
     return `Already in /home. Try 'ls' for list of directories.`;
   }
   return `Only Parseltongue speakers may enter here.`;
