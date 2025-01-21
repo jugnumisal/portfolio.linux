afbf1ef9a3139544647029abb1e929da89741577
Jugnu Misal <jugnumisal@gmail.com>
2025-01-20 20:44:44 -0600

    adding sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://www.jugnumisal.com',
+    generateRobotsTxt: true,             // Optional: Automatically generates robots.txt file
+    sitemapSize: 7000,                   // Optional: Adjusts the max number of URLs per sitemap file
+    changefreq: 'daily',                 // Optional: Frequency of page changes
+    priority: 0.7,                       // Optional: Default priority for pages
+  };
+  
